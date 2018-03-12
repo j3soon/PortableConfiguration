02c534d76738e1c7f382fd2b5cd80224a733a060
Johnson <j3.soon@msa.hinet.net>
2018-03-13 00:50:42 +0800

    Add Config and Sample

---
+++ ConfigurationData.Sample/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurationData.Sample
+{
+    class Program
+    {
+        public static string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Config config = new Config(Path.Combine(AssemblyDirectory, "config"));
+            config.DefaultConfigEvent += () =>
+            {
+                config["test1"] = "1";
+                config["test2"] = "2";
+                try
+                {
+                    config["1test"] = "1";
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Caught");
+                }
+            };
+            config.Load();
+            foreach (var pair in config.Enumerate())
+                Console.WriteLine(pair.Key + ":" + pair.Value);
+            config.Save();
+            Console.Read();
+        }
+    }
+}
---
+++ ConfigurationData/Config.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ConfigurationData
+{
+    public class Config
+    {
+        private String _path = null;
+        private SortedDictionary<String, String> _dict = new SortedDictionary<String, String>(StringComparer.OrdinalIgnoreCase);
+        private Regex _keyRegex = new Regex("^[a-zA-Z_][a-zA-Z_0-9]*$");
+
+        public delegate void DefaultConfigEventHandler();
+        public event DefaultConfigEventHandler DefaultConfigEvent;
+
+        public String this[String s]
+        {
+            get { return _dict[s]; }
+            set
+            {
+                if (String.IsNullOrEmpty(s))
+                    throw new ArgumentException();
+                if (!_keyRegex.IsMatch(s))
+                    throw new ArgumentException();
+                _dict[s] = value;
+            }
+        }
+
+        public Config(string path)
+        {
+            FileInfo f = new FileInfo(path);
+            _path = f.FullName;
+        }
+
+        public void Load()
+        {
+            DefaultConfigEvent?.Invoke();
+            if (!File.Exists(_path))
+                File.Create(_path).Close();
+            StreamReader sr = new StreamReader(_path);
+            String key = "", value = "";
+            //0->key
+            //1->val
+            int state = 0;
+            int ret;
+            char c;
+            while (true)
+            {
+                ret = sr.Read();
+                c = (char)ret;
+                if (ret == '\r' || ret == '\n' || ret == -1)
+                {
+                    //Save if key isn't empty.
+                    if (key != "")
+                    {
+                        this[key] = value;
+                        state = 0;
+                        key = value = "";
+                    }
+                    if (ret == -1)
+                        break;
+                }
+                else if (state == 0 && c == ':')
+                    state = 1;
+                else if (state == 0)
+                    key += c;
+                else
+                    value += c;
+            }
+            sr.Close();
+        }
+
+        public void Save()
+        {
+            FileInfo f = new FileInfo(_path);
+            Directory.CreateDirectory(Path.GetDirectoryName(_path));
+            StreamWriter sw = new StreamWriter(_path);
+            foreach (KeyValuePair<String, String> pair in _dict)
+                sw.WriteLine(pair.Key + ":" + pair.Value);
+            sw.Flush();
+            sw.Close();
+        }
+
+        public IEnumerable<KeyValuePair<String, String>> Enumerate()
+        {
+            foreach (KeyValuePair<String, String> pair in _dict)
+                yield return pair;
+        }
+    }
+}
